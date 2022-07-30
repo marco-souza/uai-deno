148fbaa1389de47585eab98c3450e8f587eb4727
Marco Antônio <ma.souza.junior@gmail.com>
2022-07-30 20:30:12 -0300

    fea: use deno templates with -t flag

--- services/degit.ts
+++ services/degit.ts
@@ -9,8 +9,9 @@ export function addDegitHandler(cli: Command) {
 		.option('-b, --branch [branch:string]', 'Specify a branch to clone', {
 			default: 'main',
 		})
-		.option('-v, --verbose', 'Verbose output')
 		.option('-g, --git', 'Initialize git output')
+		.option('-t, --template', 'use deno templates from github.com/marco-souza/deno-templates')
+		.option('-v, --verbose', 'Verbose output')
 		.arguments('<repo:string> [folder:string]')
 		.action(cliHandler);
 }
@@ -19,25 +20,24 @@ interface IOptions {
 	branch: string;
 	git: boolean;
 	verbose: boolean;
+	template: boolean;
 }
 
 async function cliHandler(
-	{ git: enableGit, branch, verbose }: IOptions,
+	{ git: enableGit, branch, verbose, template }: IOptions,
 	repo: string,
 	folder?: string,
 ) {
-	const [username, project, ...path] = repo.split('/');
-	if (project == null) throw Error('You need to specify a project');
-
+	const [username, project, path] = parseRepositoryInfo(repo, template)
 	const githubRepoUrl = `git@github.com:${username}/${project}.git`;
-	const distFolder = folder ?? path.at(-1) ?? project;
+	const distFolder = folder ?? path.split('/').at(-1) ?? project;
 
 	$.verbose = verbose;
 
 	console.log(`🔬 Cloning repo ${distFolder}...`);
 	await $`
     git clone -b ${branch} ${githubRepoUrl} /tmp/degit-cache && \
-    mv /tmp/degit-cache/${path?.join('/') ?? ''} ${distFolder} && \
+    mv /tmp/degit-cache/${path} ${distFolder} && \
     rm -rf ${distFolder}/.git
     rm -rf /tmp/degit-cache
   `;
@@ -52,4 +52,17 @@ async function cliHandler(
 
 	console.log(`\n🦕 Done! You're ready to fly! 🚀`);
 	console.log(`\ncd ${distFolder} `);
+}
+
+type RepositoryInfo = [username: string, project: string, path: string]
+
+function parseRepositoryInfo(repo: string, templates: boolean = false): RepositoryInfo {
+	const repoSplitted = repo.split('/')
+	if (templates)
+		return ['marco-souza', 'deno-templates', repo]
+
+	const [username, project, ...path] = repoSplitted;
+	if (project == null) throw Error('You need to specify a project');
+
+	return [username, project, path.join('/')]
 }

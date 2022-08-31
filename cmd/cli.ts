1a401de24751b8fec676ed8caecfebed625a0a51
Marco Antônio <ma.souza.junior@gmail.com>
2022-08-30 21:46:24 -0300

    fea: add agnostic package manager
    usage: uai pkg

--- cmd/cli.ts
+++ cmd/cli.ts
@@ -1,6 +1,7 @@
 import { Command } from 'cliffy';
 import { addDegitHandler } from '../services/degit.ts';
 import { addDolCmd } from '../services/dol.ts';
+import { addPkgCmd } from '../services/pkg.ts';
 
 /**
  * Main CLI command, as of right now the CLI does not have sub-commands.
@@ -21,6 +22,7 @@ function makeCommand() {
 	// Add sub-commands
 	addDegitHandler(mainCli);
 	addDolCmd(mainCli);
+	addPkgCmd(mainCli);
 
 	return mainCli;
 }
---
+++ services/pkg.ts
@@ -0,0 +1,98 @@
+import { Command } from 'cliffy';
+import { $ } from 'zx';
+
+export function addPkgCmd(cli: Command) {
+	const pkgCmd = new Command()
+		.alias('p')
+		.description('Agnostic Package Manager')
+		// install
+		.command('i, install [...packages]', 'install packages')
+		.action(makeCommandHandler('install'))
+		// remove
+		.command('r, remove [...packages]', 'remove packages installed')
+		.action(makeCommandHandler('remove'))
+		// search
+		.command('s, search [query]', 'search with optional query')
+		.action(makeCommandHandler('search'))
+		// update
+		.command('u, update', 'update installed packages')
+		.action(makeCommandHandler('update'));
+
+	// bind sub-command
+	cli.command('pkg', pkgCmd)
+		.action(() => pkgCmd.showHelp());
+}
+
+type IOptions = {};
+
+const makeCommandHandler = (cmd: PkgCommands) =>
+async (
+	_opts: IOptions,
+	packages: string[],
+) => {
+	const pkg = await findPkgCommands();
+	if (pkg == null) return;
+
+	let evalCmd = pkg[cmd];
+	if (['install', 'remove'].includes(cmd)) {
+		evalCmd += ` ${(packages ?? []).join(' ')}`;
+	}
+
+	try {
+		await $`eval ${evalCmd}`;
+	} catch (error) {
+		console.error('%c[error]', 'color: tomato;', 'Something when wrong');
+	}
+};
+
+type IPkgManCommands = {
+	install: string;
+	search: string;
+	update: string;
+	remove: string;
+};
+
+type PkgCommands = keyof IPkgManCommands;
+
+type PkgManMap = {
+	yay: IPkgManCommands;
+	pamac: IPkgManCommands;
+	apt: IPkgManCommands;
+	// brew: IPkgManCommands;
+};
+
+const pkgManMap: PkgManMap = {
+	apt: {
+		install: 'sudo apt install -y',
+		search: 'sudo apt search',
+		update: 'sudo apt update; sudo apt upgrade;',
+		remove: 'sudo apt remove -y',
+	},
+	yay: {
+		install: 'yay -S --noconfirm',
+		search: 'yay -Ss',
+		update: 'yay -Syu',
+		remove: 'yay -Rssc',
+	},
+	pamac: {
+		install: 'sudo pamac install',
+		search: 'sudo pamac search',
+		update: 'sudo pamac update',
+		remove: 'sudo pamac remove',
+	},
+};
+
+async function findPkgCommands() {
+	for (const pm in pkgManMap) {
+		if (await commandExists(pm)) return pkgManMap[pm as keyof PkgManMap];
+	}
+}
+
+async function commandExists(cmd: string) {
+	try {
+		const out = await $`command -v ${cmd}`;
+		return out.stdout.length > 0;
+	} catch (e) {
+		return false;
+	}
+}
